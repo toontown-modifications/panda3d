5cff8d82fb299c0bfdf02c05b157b8ead774865b
John Cote <jwcotejr@gmail.com>
2019-01-25 23:37:36 -0500

    toontown: Properly name DNA parser & lexer files
    Let's try this again

--- panda/src/toontown/build.sh
+++ panda/src/toontown/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-flex -Pdnayy -o dna_lxx.cxx dna.lxx
-bison -y -d -p dnayy -o dna_yxx.cxx dna.yxx
+flex -Pdnayy -o dnaLexer.cxx.prebuilt dnaLexer.lxx
+bison -y -d -p dnayy -o dnaParser.cxx.prebuilt dnaParser.yxx
