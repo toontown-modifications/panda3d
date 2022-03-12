d8ec1ca04accf07634c357b2e921e49ca6b8c6da
Thunderzapper <justinabossmlg@gmail.com>
2022-03-12 15:08:33 -0500

    update build script

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 title - Panda3D Builder
-color 4
+color 2
 :main
-makepanda\makepanda.bat --everything --installer --msvc-version=14.2 --no-eigen --windows-sdk 10 --threads=4
+makepanda\makepanda.bat --everything --installer --msvc-version=14.3 --windows-sdk=10 --no-eigen --threads=4
 echo Done!
 pause
 goto :main
